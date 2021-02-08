7ac6b9c153b8e8c5ca2c56d08def067926a3b6ba
Hilary Richardson <hilary.richardson@gmail.com>
2021-02-07 20:31:47 -0800

    adding timing functionality and colors for expiration

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,38 +1,75 @@
+
 //use uquery and moment.js to update id currentDay to display day of week, Month, and calendar day (e.g., 'Thursday, September 5th') based on current day
-
-
 $('#currentDay').text(moment().format('dddd, MMMM Do'));
 
-$('#currentDay')
+// update elements based on time of day
+$('#currentHour')
+var tasks = {};
 
-// var dt = moment(myDate.date, "YYYY-MM-DD HH:mm:ss")
-// dt.format('dddd');
-// console.log(moment(mydate).format('dddd'));
+//save text from input
+$('.input').on('click', function () {
+    var text = $(this).text().trim();
+    console.log(text);
 
+});
 
+// save button triggers saving task in local storage
+$(".saveBtn").click(function () {
+    console.log("button is working")
+    // get form values based on closest element and class
+    var taskText = $(this).siblings(".taskDescription").val();
+    console.log(taskText);
 
-// $(document).ready(function () {
-//     $('#calendar').eCalendar({
+    // create a variable of task date. assigning the 
+    // value of the element w/class dueDate closest to the button 
+    // we clicked
+    var taskDate = $(this).siblings(".dueDate").text().trim();
+    console.log(taskDate);
 
-//         weekDays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    if (taskText && taskDate) {
+        localStorage.setItem(taskDate, JSON.stringify(taskText));
+        console.log("task data is stored!");
+    }
+    else {
+        event.preventDefault()
+        console.log("no task data was stored");
+    }
+});
 
-//         months: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+// unstringify the JSON value when page loads
+// on page load check local storage, if if (taskText && taskDate) then unstringify
 
-//         textArrows: { previous: '<', next: '>' },
+$("#nine").val(JSON.parse(localStorage.getItem("9:00")));
+$("#ten").val(JSON.parse(localStorage.getItem("10:00")));
+$("#eleven").val(JSON.parse(localStorage.getItem("11:00")));
+$("#twelve").val(JSON.parse(localStorage.getItem("12:00")));
+$("#thirteen").val(JSON.parse(localStorage.getItem("13:00")));
+$("#fourteen").val(JSON.parse(localStorage.getItem("14:00")));
+$("#fifteen").val(JSON.parse(localStorage.getItem("15:00")));
+$("#sixteen").val(JSON.parse(localStorage.getItem("16:00")));
+$("#seventeen").val(JSON.parse(localStorage.getItem("17:00")));
 
-//         eventTitle: 'Events',
+// show whether time of task deadline has passed yet
+// create variable for functionality for creating or editing task due dates
+var auditTask = function () {
+    // get hour from task element
+    // var date = $(taskEl).find("span").text().trim();
+    var startHour = $(".hour").text().trim();
 
-//         url: '',
+    // convert to moment object at 5:00pm
+    // var time = moment(date, "L").set("hour", 17);
+    var startTime = moment(startHour, "H");
 
-//         firstDayOfWeek: 0,
+    // change class if past due hour
+    if (moment().isAfter(startTime)) {
+        $(".taskDescription").removeClass("future present");
+        $(".taskDescription").addClass("past");
+    }
+    // change class if currently due
+    if (moment().isSame(startTime)) {
+        $(".taskDescription").removeClass("future past");
+        $(".taskDescription").addClass("present");
+    }
+};
 
-//         events: [
-
-//             { title: 'Event 1', description: 'Description 1', < a href = "https://www.jqueryscript.net/time-clock/" > date</a > time: new Date(2014, 7, 15, 17)},
-
-//         { title: 'Event 2', description: 'Description 2', datetime: new Date(2014, 7, 14, 16) },
-
-//         { title: 'Event 3', description: 'jQueryScript.Net', datetime: new Date(2014, 7, 10, 16) }
-
-//     ]});
-//     });
+auditTask();
